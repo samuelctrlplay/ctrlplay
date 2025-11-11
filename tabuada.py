905fbc0fcde87930fb822ec9f9d0b2279aeedb80
Samuel <samuelthallesctrlplay@gmail.com>
2025-11-11 15:46:55 -0300

    Meu primeiro commit

--- tabuada.py
+++ tabuada.py
@@ -1,11 +1,13 @@
 def tabuada(valor, inicio = 0, fim = 10, *, imprimir = False):
+    # Troca os valores de inicio e fim caso o valor de inicio seja maior que o valor de fim
     if inicio > fim: inicio, fim = fim, inicio
-
+    # Inicia a variavel texto como string vazia
     texto = ''
-
+    # Laco for para iterar sobre os valores inteiros a partir de inicio ate fim
     for i in range(inicio, fim + 1):
+        # Acrescenta uma linha da tabuada a texto
         texto += f'{i} X {valor} = {i * valor}\n'
-    
+    # Imprime a tabuada completa no console se o parametro imprimir for True
     if imprimir: print(texto)
-
+    # Retorna o texto gerado contendo a tabuada
     return texto
